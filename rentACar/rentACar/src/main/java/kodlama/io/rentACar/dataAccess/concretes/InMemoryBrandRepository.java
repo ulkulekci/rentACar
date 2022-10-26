9adae6a2c4da63942b0f9d5e1dfea7e6b4e20356
ulkulekci <ulkusulu07@hotmail.com>
2022-10-26 20:26:40 +0300

    Java Kampı 4.hafta (25 ekim salı)

---
+++ rentACar/rentACar/src/main/java/kodlama/io/rentACar/dataAccess/concretes/InMemoryBrandRepository.java
@@ -0,0 +1,27 @@
+package kodlama.io.rentACar.dataAccess.concretes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
+import kodlama.io.rentACar.entities.concretes.Brand;
+
+public class InMemoryBrandRepository implements BrandRepository{ 
+	
+	List<Brand> brands; //globalda tanımladım getAll dediğim anda erişiliyor
+	
+	public InMemoryBrandRepository() {
+		brands = new ArrayList<Brand>(); //veri tabanı eklemedim inmemory çalışıyorum bunu kullanan için boş bir array oluşturdum uyduruktan marka listesi
+		brands.add(new Brand(1, "BMV")); //veri tabanına eklemek yerine sanki veri tabanından geliyormuş gibi
+		brands.add(new Brand(2, "Mercedes"));
+		brands.add(new Brand(3, "Audi"));
+		brands.add(new Brand(4, "Fiat"));
+		brands.add(new Brand(5, "Renault"));
+	}
+
+	public List<Brand> getAll() { //içinde markalar barındıran listeler döndürücem demek
+		return brands;
+		
+	}
+	
+}
